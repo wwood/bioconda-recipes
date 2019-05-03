60a624b64b9de52c30142357dd24a431421f4be8
aemiol <38737287+aemiol@users.noreply.github.com>
2019-05-02 21:14:59 -0400

    Revert "New smeg (#15036)"
    This reverts commit 01b61f02fdf43315436f490f8045abbb4c50a1c7.

--- recipes/smeg/build.sh
+++ recipes/smeg/build.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-set -eu -o pipefail
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
@@ -7,15 +5,9 @@ mkdir -p $PREFIX/bin
 
 cd $SRC_DIR
 
-cp -R dnaA_database $outdir
-cp *.R $outdir/
-cp smeg README.md growth_est_denovo growth_est_ref build_sp pileupParser uniqueSNPmultithreading uniqueClusterSNP $outdir
+cp -a . /$outdir/
 chmod +x $outdir/smeg
 chmod +x $outdir/uniqueSNPmultithreading
 chmod +x $outdir/pileupParser
-chmod +x $outdir/growth_est_denovo
-chmod +x $outdir/growth_est_ref
-chmod +x $outdir/build_sp
-chmod +x $outdir/uniqueClusterSNP
 
 ln -s $outdir/smeg $PREFIX/bin/smeg

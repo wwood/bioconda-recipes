957ae265dd689279384f6df788e68d440ba2b4e2
Marcel Martin <marcel.martin@scilifelab.se>
2019-06-24 11:27:31 +0200

    wip

--- recipes/ncbi-vdb/build.sh
+++ recipes/ncbi-vdb/build.sh
@@ -1,10 +1,23 @@
+# Debian's ncbi-vdb package: https://salsa.debian.org/med-team/ncbi-vdb
 
-sed -i.backup -e "s|gcc|$CC|g" -e "s|g++|$CXX|g" build/Makefile.gcc
+# Paths to toolchain binaries (gcc, g++, ar) are all hard-coded and need some
+# patching.
 
+sed -i.backup \
+    -e "s|gcc|$CC|g" \
+    -e "s|g++|$CXX|g" \
+    build/Makefile.gcc
 ./configure \
     --prefix=$PREFIX \
     --build-prefix=ncbi-outdir \
     --with-ngs-sdk-prefix=$PREFIX
+sed -i.backup \
+    -e "s|= gcc|= $CC|" \
+    -e "s|= gcc|= $CXX|" \
+    -e "s|= ar rc|= ar|" \
+    -e "s|= ar|= $AR|" \
+    build/Makefile.config.linux.x86_64
+
 make
 make install
 make -C test/vdb
